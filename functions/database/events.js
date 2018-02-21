78427a659799a963c0db1edf00fff4b98e59b5bf
Nicolas Chalet <nicolas.chalet@gmail.com>
2018-02-21 01:21:59 +0100

    feat(database): added two-way databinding between user and event

--- functions/database/events.js
+++ functions/database/events.js
@@ -29,3 +29,27 @@ exports.onPublicDataCreate = functions.database.ref('events/publicData/{eventId}
       })
     ]);
   });
+
+exports.onMemberRoleChange = functions.database.ref('events/memberData/{eventId}/members/{uid}')
+  .onWrite((event) => {
+    const isMember = event.data.val();
+    console.log(`Member role changed... eventId: ${event.params.eventId}, uid: ${event.params.uid}. New Value: ${isMember}`);
+
+    if (!isMember) {
+      return event.data.adminRef.root.child(`users/${event.params.uid}/events/member/${event.params.eventId}`).remove();
+    }
+
+    return event.data.adminRef.root.child(`users/${event.params.uid}/events/member/${event.params.eventId}`).set(true);
+  });
+
+exports.onAdminRoleChange = functions.database.ref('events/adminData/{eventId}/admins/{uid}')
+  .onWrite((event) => {
+    const isAdmin = event.data.val();
+    console.log(`Admin role changed... eventId: ${event.params.eventId}, uid: ${event.params.uid}. New Value: ${isAdmin}`);
+
+    if (!isAdmin) {
+      return event.data.adminRef.root.child(`users/${event.params.uid}/events/admin/${event.params.eventId}`).remove();
+    }
+
+    return event.data.adminRef.root.child(`users/${event.params.uid}/events/admin/${event.params.eventId}`).set(true);
+  });
