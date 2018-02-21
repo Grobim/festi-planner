0692703e0a4e49ea572a7046111b54beb621c12a
Nicolas Chalet <nicolas.chalet@gmail.com>
2018-02-21 02:28:01 +0100

    feat(user): added eventName to relationship

--- functions/database/events.js
+++ functions/database/events.js
@@ -1,18 +1,50 @@
 const functions = require('firebase-functions');
 const Promise = require('promise');
 
-exports.lowercaseName = functions.database.ref('events/publicData/{eventId}/name')
+exports.onEventNameWrite = functions.database.ref('events/publicData/{eventId}/name')
   .onWrite((event) => {
-    if (!event.data.exists()) {
-      console.log('Not lowercasing', event.params.eventId, 'reason: !event.data.exists()');
-      return null;
+    const promises = [];
+    const newEventName = event.data.val();
+
+    if (newEventName) {
+      console.log('Lowercasing', event.params.eventId, newEventName);
+      const lowercase = newEventName.toLowerCase();
+
+      promises.push(event.data.adminRef.parent.child('lowercaseName').set(lowercase));
     }
 
-    const original = event.data.val();
-    console.log('Lowercasing', event.params.eventId, original);
-    const lowercase = original.toLowerCase();
+    return Promise.all([
+      event.data.adminRef.root.child(`events/memberData/${event.params.eventId}/members`).once('value'),
+      event.data.adminRef.root.child(`events/adminData/${event.params.eventId}/admins`).once('value')
+    ]).then((roles) => {
+      const memberRolesSnap = roles[0];
 
-    return event.data.adminRef.parent.child('lowercaseName').set(lowercase);
+      if (memberRolesSnap.exists()) {
+        console.log(`Updating event name for members: ${JSON.stringify(memberRolesSnap.val())}`);
+
+        memberRolesSnap.forEach((snap) => {
+          if (snap.val() === true) {
+            promises.push(event.data.adminRef.root
+              .child(`users/${snap.key}/events/member/${event.params.eventId}/eventName`)
+              .set(newEventName));
+          }
+        });
+      }
+
+      const adminRolesSnap = roles[1];
+
+      if (adminRolesSnap.exists()) {
+        console.log(`Updating event name for admins: ${JSON.stringify(adminRolesSnap.val())}`);
+
+        adminRolesSnap.forEach((snap) => {
+          if (snap.val() === true) {
+            promises.push(event.data.adminRef.root
+              .child(`users/${snap.key}/events/admin/${event.params.eventId}/eventName`)
+              .set(newEventName));
+          }
+        });
+      }
+    }).then(() => Promise.all(promises));
   });
 
 exports.onPublicDataCreate = functions.database.ref('events/publicData/{eventId}')
@@ -30,26 +62,38 @@ exports.onPublicDataCreate = functions.database.ref('events/publicData/{eventId}
     ]);
   });
 
-exports.onMemberRoleChange = functions.database.ref('events/memberData/{eventId}/members/{uid}')
+exports.onMemberRoleWrite = functions.database.ref('events/memberData/{eventId}/members/{uid}')
   .onWrite((event) => {
     const isMember = event.data.val();
     console.log(`Member role changed... eventId: ${event.params.eventId}, uid: ${event.params.uid}. New Value: ${isMember}`);
 
+    const roleRef = event.data.adminRef.root.child(`users/${event.params.uid}/events/member/${event.params.eventId}`);
     if (!isMember) {
-      return event.data.adminRef.root.child(`users/${event.params.uid}/events/member/${event.params.eventId}`).remove();
+      return roleRef.remove();
     }
 
-    return event.data.adminRef.root.child(`users/${event.params.uid}/events/member/${event.params.eventId}`).set(true);
+    return event.data.adminRef.root.child(`events/publicData/${event.params.eventId}/name`).once('value')
+      .then(snap => snap.val())
+      .then(eventName => roleRef.set({
+        isMember: true,
+        eventName
+      }));
   });
 
-exports.onAdminRoleChange = functions.database.ref('events/adminData/{eventId}/admins/{uid}')
+exports.onAdminRoleWrite = functions.database.ref('events/adminData/{eventId}/admins/{uid}')
   .onWrite((event) => {
     const isAdmin = event.data.val();
     console.log(`Admin role changed... eventId: ${event.params.eventId}, uid: ${event.params.uid}. New Value: ${isAdmin}`);
 
+    const roleRef = event.data.adminRef.root.child(`users/${event.params.uid}/events/admin/${event.params.eventId}`);
     if (!isAdmin) {
-      return event.data.adminRef.root.child(`users/${event.params.uid}/events/admin/${event.params.eventId}`).remove();
+      return roleRef.remove();
     }
 
-    return event.data.adminRef.root.child(`users/${event.params.uid}/events/admin/${event.params.eventId}`).set(true);
+    return event.data.adminRef.root.child(`events/publicData/${event.params.eventId}/name`).once('value')
+      .then(snap => snap.val())
+      .then(eventName => roleRef.set({
+        isAdmin: true,
+        eventName
+      }));
   });
