c21f36f935bd15be9ecb9e138f4c92f52fabf1c7
Nicolas Chalet <nicolas.chalet@gmail.com>
2018-02-25 23:39:28 +0100

    feat(app): renamed app to Festi Planner, changed deployed url

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -101,7 +101,7 @@ class AppBar extends Component {
           </IconButton>
           <Typography className={classes.title} variant="title" color="inherit">
             <Link href="/" to="/">
-              FckNyE - Planner
+              Festi - Planner
             </Link>
           </Typography>
           {isConnected ? (
